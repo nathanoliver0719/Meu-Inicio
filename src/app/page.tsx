4121cbdcd9cdb1cee177f3375fc8401a84db29a1
nathanoliver0719 <87840418+nathanoliver0719@users.noreply.github.com>
2024-09-27 17:39:48 -0300

    Enviando arquivo para o GitHub

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,207 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    
+    
+<body className="bg-gray-100 text-gray-600">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  <header>
+    <div className="container mx-auto px-4 h-24 flex items-center justify-between">
+      
+      <div className="font-black">
+        LOGO
+      </div>
+      
+      <ul className="flex gap-3">
+
+          <li><a href="" className="text-sm text-gray-600 hover:text-gray-800 hover:underline">Como reservar</a></li>
+          <li><a href="" className="text-sm text-gray-600 hover:text-gray-800 hover:underline">Sobre nós</a></li>
+          <li><a href="" className="text-sm text-gray-600 hover:text-gray-800 hover:underline">Informações</a></li>
+          <li><a href="" className="text-sm text-gray-600 hover:text-gray-800 hover:underline">Contato</a></li>
+
+      </ul>
     </div>
+
+  </header>
+
+{/* Inicio do banner >>> */}
+
+  <section>
+    <div className="container mx-auto px-4 ">
+
+      
+
+      <div className="relative flex items-center justify-center">
+        <img className="h-[450px] " src="/img/hero-image-1.png" alt="" />
+        
+        {/* Conteúdo dentro da imagem (INICIO) */}
+        <div className="absolute flex justify-center flex-wrap space-y-20">
+
+          {/* Texto da imagem (INICIO) */}
+
+          <div className="text-center">
+            <h1 className="text-gray-100 text-3xl mb-2">Onde você quer ir?</h1>
+            <p className="text-green-100">Escolha o estado e cidade para achar os melhores resorts.</p>
+          </div>
+          {/* Texto da imagem (FIM) */}
+
+          {/* FORMS >>>*/}
+
+          <div className="absolute ">
+
+            <form className="flex space-x-4 mt-7">
+              <select className="bg-gray-50 px-3 py-2 border border-gray-300 rounded text-gray-500 ">
+                <option value="">Escolha o estado</option>
+              </select>
+
+              <select className="bg-gray-50 px-3 py-2 border border-gray-300 rounded text-gray-500">
+                <option value="">Escolha a cidade</option>
+              </select> 
+
+              <button className="bg-yellow-600 text-yellow-950 font-medium px-3 py-2 rounded text-nowrap flex items-center gap-1">
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                </svg>
+
+                Pesquisar resorts
+              </button>
+
+            </form>
+          
+          </div>
+
+          {/* <<< FORMS */}
+
+
+        </div>
+        {/* Conteúdo dentro da imagem (FIM)*/} 
+
+        
+      </div>
+         
+
+    </div>
+  </section>
+
+{/* <<< FIM do banner */}
+
+
+{/* Painel (inicio) >>> */}
+
+    <section>
+      <div className="container mx-auto px-4 py-10">
+
+        {/* Texto >> */}
+        <main className="flex">
+            
+          <div className="space-y-3 pr-10">
+            
+            <h2 className="text-sm text-gray-400 font-medium ">Resort em destaque</h2>
+
+            <h1 className="text-2xl">Ecoresort Praia do Forte Bahia</h1>
+
+            <p className="text-sm text-gray-400 font-medium  ">Hotel qualidade 5 estrelas</p>
+
+            <div className="text-sm text-gray-400 font-medium space-y-2   ">
+              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit molestiae fuga voluptates. Assumenda, excepturi! Error beatae iure, cupiditate omnis eius saepe, voluptatum accusantium, ut iusto minus consequuntur! Dolor, dolore fuga.</h3>
+
+              <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, neque consectetur provident quos possimus consequuntur quaerat obcaecati magni natus recusandae porro ullam optio delectus ducimus ipsa incidunt exercitationem ut quisquam.</h3>
+            </div>
+
+            <button className="bg-yellow-600 text-yellow-950 font-medium px-3 py-2 rounded text-nowrap flex items-center gap-4">
+              
+              Solicitar Reserva
+
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+              </svg>
+
+              
+            </button>
+
+          </div>
+          {/* << Texto */}
+
+
+
+          {/* Imagem >> */}
+
+          <div className="ml-auto flex-shrink-0">
+          
+            <img className="w-[450px] " src="/img/praia-do-forte-820x547.jpg" alt="" />
+            
+          </div>
+          {/* << imagem */}
+
+        </main>
+
+        
+      </div>
+    </section>
+
+
+{/* <<< Painel (fim) */}
+
+
+{/* grid de fotos (inicio) >>> */}
+
+    <section>
+      <main className="container mx-auto px-4 ">
+
+        <div className="space-y-8">
+          
+          <h3 className="text-2xl text-center font-semibold">Conheça mais Resorts</h3>
+
+          <div className="grid grid-cols-4 grid-rows-2 gap-6">
+
+            <div className="col-span-2 row-span-2 relative">
+              <img className="w-full h-full object-cover rounded overflow-hidden" src="/grid/hotel_1.jpg"/>
+              <div className="absolute bottom-0 pl-4 pb-2 z-20"><h1 className="text-white text-base font-medium">Beach Park Resort</h1></div>
+              <img className="absolute h-20 bottom-0 left-0 right-0 z-10" src="/img/image-shadow.png"/>
+
+            </div>
+
+            <div className="col-span-2 h-40 relative">
+              <img className="w-full h-full object-cover rounded overflow-hidden" src="/grid/hotel_2.jpg"/>
+              <div className="absolute bottom-0 pl-4 pb-1 z-20"><h1 className="text-white text-base font-medium">Fortal Resort</h1></div>
+              <img className="absolute h-20 bottom-0 left-0 right-0 z-10" src="/img/image-shadow.png"/>
+
+            </div>
+
+            <div className=" relative ">
+              <img className="w-full h-full object-cover rounded overflow-hidden" src="/grid/hotel_3.jpg"/>
+              <div className="absolute bottom-0 pl-4 pb-1 z-20"><h1 className="text-white text-base font-medium">Miami Beach Hotel Resort</h1></div>
+              <img className="absolute h-20 bottom-0 left-0 right-0 z-10" src="/img/image-shadow.png"/>
+
+            </div>
+            
+            <div className=" relative ">
+              <img className="w-full h-full object-cover rounded overflow-hidden" src="/grid/hotel_4.jpg"/>
+              <div className="absolute bottom-0 pl-4 pb-1 z-20"><h1 className="text-white text-base font-medium">Resort Maxaranguape </h1></div>
+              <img className="absolute h-20 bottom-0 left-0 right-0 z-10" src="/img/image-shadow.png"/>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </main>
+    </section>
+
+
+
+
+    <div><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /></div>
+
+
+
+
+{/* <<< grid de fotos (fim) */}
+
+
+
+</body>
   );
 }
